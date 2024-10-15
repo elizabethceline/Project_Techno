32f22bfbe24c5eb0b577ffcf2441aa0b8145421f
elizabethceline <elizabethceline04@gmail.com>
2024-10-15 22:04:43 +0700

    feat: myride & track

---
+++ track.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EduRide</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/tw-elements.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/css/tw-elements.min.css" />
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css">
+
+</head>
+
+<body>
+    <img src="assets/img/track.png" class="w-screen h-screen" alt="">
+    <div class="absolute top-4 left-4 bg-white bg-opacity-95 w-10 h-10 flex items-center justify-center rounded-full z-10 shadow-md">
+        <i class="fa-solid fa-arrow-left fa-lg text-neutral-800"></i>
+    </div>
+
+    <?php include 'include/script.php'; ?>
+</body>
+
+</html>
